89e4f762ffda93c7cb91f046855af46cbc73a952
franciscovelasquezg-dotcom <franciscovelasquezg@gmail.com>
2026-01-06 20:47:44 -0300

    Add files via upload

--- finance_pro.py
+++ finance_pro.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
 import socket
 import hashlib
